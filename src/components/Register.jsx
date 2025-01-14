af2ab221e0d34bbad408bbc8fb6d45dd5ce94e4a
farukycgl <faruk.kuy@gmail.com>
2025-01-15 01:26:02 +0300

    Register.jsx created

---
+++ src/components/Register.jsx
@@ -0,0 +1,116 @@
+import { Button, Card, CardBody, CardHeader, Form, FormFeedback, FormGroup, Input, Label } from "reactstrap";
+import { useState, useEffect } from "react";
+
+const initialValues = {
+    email: "",
+    password: "",
+}; 
+
+const errorMessages = {
+    email: "Geçerli bir email adresi giriniz!",
+    password: "En az 8 karakter, en az 1 büyük harf, en az 1 küçük harf, en az 1 rakam ve sembol içermelidir.",
+}
+
+const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
+
+let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+
+export default function Register() {
+    const [formData, setFormData] = useState(initialValues);
+    const [errors, setErrors] = useState({
+        email: false,
+        password: false,
+    });
+    const [isValid, setIsValid] = useState(false);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+
+        if (name === "email") {
+            if (validateEmail(value)) {
+                setErrors({ ...errors, [name]: false });
+            } else {
+                setErrors({ ...errors, [name]: true });
+            };
+        };
+
+        if (name === "password") {
+            if (regex.test(value)) {
+                setErrors({ ...errors, [name]: false });
+            } else {
+                setErrors({ ...errors, [name]: true });
+            }
+        }
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!isValid) return;
+    };
+
+    useEffect(() => {
+
+        if (validateEmail(formData.email) && regex.test(formData.password)) {
+            setIsValid(true);
+        } else {
+            setIsValid(false);
+        };
+
+    }, [formData]);
+
+
+    return (
+        <Card>
+            <CardHeader>
+                KAYIT OL
+            </CardHeader>
+            <CardBody>
+                <Form onSubmit={handleSubmit}>
+                    <FormGroup>
+                        <Label for="email">
+                            Email
+                        </Label>
+                        <Input
+                            id="email"
+                            name="email"
+                            placeholder="Email adresinizi giriniz"
+                            type="email"
+                            onChange={handleChange}
+                            value={formData.email}
+                            invalid={errors.email}
+                        />
+                        {errors.email && <FormFeedback>{errorMessages.email}</FormFeedback>}
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="password">
+                            Password
+                        </Label>
+                        <Input
+                            id="password"
+                            name="password"
+                            placeholder="Güçlü bir şifre giriniz"
+                            type="password"
+                            onChange={handleChange}
+                            value={formData.password}
+                            invalid={errors.password}
+                        />
+                        {errors.password && <FormFeedback>{errorMessages.password}</FormFeedback>}
+                    </FormGroup>
+                    <Button disabled={!isValid}>
+                        KAYIT OL
+                    </Button>
+                </Form>
+            </CardBody>
+        </Card>
+    )
+}
